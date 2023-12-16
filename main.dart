4d46d81f1a0be7a66f8ade5b3657e064f20bc2cd
fwnpx <153355563+fwnpx@users.noreply.github.com>
2023-12-16 23:58:16 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
+// Top-level function
 void main() {
   runApp(const MyApp());
 }
@@ -7,7 +10,56 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  AppBar _buildAppBar() {
+    var firstName = 'Wannaporn';
+    var lastName = 'Onsamlee';
+    var age = 22;
+
+    // String interpolation
+    return AppBar(
+      title: Text('$firstName $lastName, age: $age'),
+    );
+  }
+
+  Widget _buildPageBody() {
+    List<String> image = [
+      'assets/images/lovepik-seawater-png-image_401259758_wh1200.png',
+      'assets/images/sky.png',
+      'assets/images/R.png'
+    ];
+    List<String> word = ['Sea', 'Sky', 'Mountain'];
+    Random random = Random();
+
+    // Generate a random number between 1 and 3
+    int randomNumber = random.nextInt(2) + 1;
+
+    return  Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            image[randomNumber],
+            width: 200.0,
+            fit: BoxFit.contain,
+          ),
+          Text(
+            word[randomNumber],
+            // Named parameter
+            style: TextStyle(
+              fontSize: 80.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+            ),
+            // Named parameter
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Method
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,10 +69,8 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: Scaffold(
-        appBar: AppBar(title: Center(child: Text('Wannaporn Onsamlee'))),
-        body: Center(
-          child: Text('ชื่อ:วรรณพร อ่อนสำลี\nรหัสนักศึกษา: 630710675'),
-        ),
+        appBar: _buildAppBar(),
+        body: _buildPageBody(),
       ),
     );
   }
